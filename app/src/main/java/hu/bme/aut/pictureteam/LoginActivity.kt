ecaabbf7e0977b92484e63209790dc12d2ffae48
Tamas <kalvintamasendre@gmail.com>
2020-10-05 12:49:49 +0200

    feat: pictures basic tabs layout

---
+++ app/src/main/java/hu/bme/aut/pictureteam/LoginActivity.kt
@@ -0,0 +1,33 @@
+package hu.bme.aut.pictureteam
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_login.*
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        try {
+            Thread.sleep(1000)
+        } catch (e: InterruptedException) {
+            e.printStackTrace()
+        }
+        setTheme(R.style.AppTheme)
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+
+        btnLogin.setOnClickListener {
+            if (etEmailAddress.text.toString().isEmpty()) {
+                etEmailAddress.requestFocus()
+                etEmailAddress.error = "Please enter your email address"
+            }
+            else if (etPassword.text.toString().isEmpty()) {
+                etPassword.requestFocus()
+                etPassword.error = "Please enter your password"
+            }
+            else {
+                startActivity(Intent(this, MainActivity::class.java))
+            }
+        }
+    }
+}
--- app/src/main/java/hu/bme/aut/pictureteam/MainActivity.kt
+++ app/src/main/java/hu/bme/aut/pictureteam/MainActivity.kt
@@ -1,37 +1,25 @@
 package hu.bme.aut.pictureteam
 
-import android.annotation.SuppressLint
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
-import android.view.LayoutInflater
-import androidx.lifecycle.lifecycleScope
-import hu.bme.aut.pictureteam.models.Instrument
-import hu.bme.aut.pictureteam.services.Api
-import kotlinx.android.synthetic.main.activity_main.*
-import kotlinx.android.synthetic.main.instruments_row.view.*
-import kotlinx.coroutines.launch
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.android.material.snackbar.Snackbar
+import com.google.android.material.tabs.TabLayout
+import androidx.viewpager.widget.ViewPager
+import androidx.appcompat.app.AppCompatActivity
+import android.view.Menu
+import android.view.MenuItem
+import hu.bme.aut.pictureteam.ui.main.SectionsPagerAdapter
 
 class MainActivity : AppCompatActivity() {
-    var instruments = listOf<Instrument>();
 
-    @SuppressLint("InflateParams")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val sectionsPagerAdapter = SectionsPagerAdapter(this, supportFragmentManager)
+        val viewPager: ViewPager = findViewById(R.id.view_pager)
+        viewPager.adapter = sectionsPagerAdapter
+        val tabs: TabLayout = findViewById(R.id.tabs)
+        tabs.setupWithViewPager(viewPager)
 
-        val context = this
-
-        lifecycleScope.launch {
-            instruments = Api.create().getInstruments(20).instruments
-
-            for (instrument in instruments) {
-                instrument.name
-                val rowItem = LayoutInflater.from(context).inflate(R.layout.instruments_row, null)
-                rowItem.row_instrument_name.text = instrument.name
-                rowItem.row_instrument_family.text = instrument.family
-                list_of_rows.addView(rowItem)
-            }
-        }
     }
 }
